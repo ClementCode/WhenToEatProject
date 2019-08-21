545a05b62aa9de73d8acef88f453d9a5c1f7ac7d
Clemounix <dubeaurepaire59@gmail.com>
2019-08-21 23:32:02 +0200

    Add the OpeningHour Entity files.

---
+++ OpeningHoursProject/src/AppBundle/Entity/OpeningHour.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OpeningHour
+ *
+ * @ORM\Table(name="opening_hour")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\OpeningHourRepository")
+ */
+class OpeningHour
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hours", type="text")
+     */
+    private $hours;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set hours
+     *
+     * @param string $hours
+     *
+     * @return OpeningHour
+     */
+    public function setHours($hours)
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+
+    /**
+     * Get hours
+     *
+     * @return string
+     */
+    public function getHours()
+    {
+        return $this->hours;
+    }
+}
+
