7ae5af04da6b286ce14a4d15ae15243cd930be12
Clemounix <dubeaurepaire59@gmail.com>
2019-08-21 23:10:27 +0200

    Add the Weekday Entity files.

---
+++ OpeningHoursProject/src/AppBundle/Entity/Weekday.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Weekday
+ *
+ * @ORM\Table(name="weekday")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\WeekdayRepository")
+ */
+class Weekday
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="day", type="string", length=16, unique=true)
+     */
+    private $day;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set day
+     *
+     * @param string $day
+     *
+     * @return Weekday
+     */
+    public function setDay($day)
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    /**
+     * Get day
+     *
+     * @return string
+     */
+    public function getDay()
+    {
+        return $this->day;
+    }
+}
+
